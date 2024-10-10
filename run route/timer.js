8a874abb59a84d76da535bf049c9e426700015e9
Hdaboom <94676358+Hdaboom@users.noreply.github.com>
2024-10-10 18:16:36 +0100

    changed timer script to not show -1 value

--- run route/timer.js
+++ run route/timer.js
@@ -2,11 +2,11 @@ var Nothingbtn=document.getElementById("do-nothing-btn");
 let timer_value=20
 
 function timer_reduce() {
-    timer_value-=1
     Nothingbtn.innerHTML=`Do nothing in ${timer_value}`
+    timer_value-=1
 
     if (timer_value<0) {
-        Nothingbtn.innerhtml="yay"
+        Nothingbtn.innerHTML="yay"
     }
 
     else {
