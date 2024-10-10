39ac7245e2b2a24c1980ebe1e5490afd715fb582
Hdaboom <94676358+Hdaboom@users.noreply.github.com>
2024-10-10 18:46:36 +0100

    added redirect on timeout

--- run route/timer.js
+++ run route/timer.js
@@ -5,8 +5,8 @@ function timer_reduce() {
     timer_text.innerHTML=`The Creature catches up to you in ${timer_value}`
     timer_value-=1
 
-    if (timer_value<0) {
-        timer_text.innerHTML="yay"
+    if (timer_value<-1) {
+        window.location.href="https://hdaboom.github.io/text-adventure/run%20route/timeout-death.html"
     }
 
     else {
